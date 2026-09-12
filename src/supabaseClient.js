ad9668a6500c337472b2ca203ff9cea8b51ed38d
jtczy6pfmy <jtczy6pfmy@privaterelay.appleid.com>
2026-09-12 14:30:19 -0400

    Connect inventory app to Supabase

---
+++ src/supabaseClient.js
@@ -0,0 +1,3 @@
+const CPI_SUPABASE_URL='https://lavsuaojwyzavwtbgkcs.supabase.co';
+const CPI_SUPABASE_KEY='sb_publishable_UGGiNae4Y9iLRQTaQ1za3A_rd8Amfp5';
+const cpiSupabase=window.supabase.createClient(CPI_SUPABASE_URL,CPI_SUPABASE_KEY);
